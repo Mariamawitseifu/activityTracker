cc2df6b1bfdb8fee35dab50d94456bba9b78324f
mariamawit <merrybee34@example.com>
2025-03-29 08:39:24 +0300

    bug fix

--- database/migrations/2025_03_28_111953_create_main_activities_table.php
+++ database/migrations/2025_03_28_111953_create_main_activities_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('main_activities', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('title');
-            $table->foreignUuid('inititative_id')->constrained('inititatives');
+            $table->foreignUuid('initiative_id')->constrained('initiatives');
             $table->enum('type', ['KPI', 'main activity']);
             $table->foreignUuid('measuring_unit_id')->constrained('measuring_units');
             $table->integer('weight');
