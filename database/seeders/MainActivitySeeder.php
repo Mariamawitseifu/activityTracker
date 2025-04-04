b90e30c6b7d03831b407f30d7ac23523721b86eb
Aman D <amandesalegnb@gmail.com>
2025-04-04 15:55:16 +0300

    manager seeder

--- database/seeders/MainActivitySeeder.php
+++ database/seeders/MainActivitySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\MainActivity;
 use App\Models\Plan;
+use App\Models\Unit;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,10 +16,12 @@ class MainActivitySeeder extends Seeder
     public function run(): void
     {
         $mainActivities = MainActivity::factory(128)->create();
+
+        $unit = Unit::where('name', 'Health System Innovation & Quality')->first();
         foreach ($mainActivities as $value) {
             Plan::updateOrCreate([
-                'main_activity_id' => $value,
-                'unit_id' => $value,
+                'main_activity_id' => $value->id,
+                'unit_id' => $unit->id,
             ]);
         }
     }
