9c80746fc784223c9a515ed24b2bb123764fb1b6
mariamawit <merrybee34@example.com>
2025-04-15 16:46:02 +0300

    bug fix

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -84,6 +84,7 @@ class PlanController extends Controller
                     'weight' => $plan->mainActivity->weight,
                     'unit' => $plan->unit->name,
                     'target' => $plan->mainActivity->target,
+                    'fiscal_year' => $plan->fiscalYear->name,
                 ];
             });
     }
@@ -191,7 +192,7 @@ public function getPlansWithSearchAndUnit($search, $unit)
                 'weight' => $plan->mainActivity->weight,
                 'unit' => $plan->unit->name,
                 'target' => $plan->mainActivity->target,
-                'fiscal_year' => $plan->fiscalYear,
+                'fiscal_year' => $plan->fiscalYear->name,
             ];
         });
 }
