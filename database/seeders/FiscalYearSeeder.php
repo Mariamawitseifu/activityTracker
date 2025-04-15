db7c690e87ae7b9159cb6d7a77998e538784149c
Aman D <amandesalegnb@gmail.com>
2025-04-15 18:01:22 +0300

    main activity seed

--- database/seeders/FiscalYearSeeder.php
+++ database/seeders/FiscalYearSeeder.php
@@ -13,11 +13,10 @@ class FiscalYearSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create 10 fiscal years
-        FiscalYear::factory(1)->create([
+        FiscalYear::firstOrCreate([
             'name' => '2023-2024',
-            'start_date' => '2023-04-01', 
-            'end_date' => '2024-03-31', 
+            'start_date' => '2023-04-01',
+            'end_date' => '2024-03-31',
         ]);
     }
 }
