dcc00b166ee04439b682b7c62b9e615fc2690b1c
mariamawit <merrybee34@example.com>
2025-04-16 10:48:40 +0300

    bug fix

--- database/migrations/2025_04_15_101424_create_monitorings_table.php
+++ database/migrations/2025_04_15_101424_create_monitorings_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('actual_value');
             $table->string('month');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
